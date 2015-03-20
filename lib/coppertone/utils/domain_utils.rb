a5f931b2202b64d7ab35d9a985becf9a3a821a65
Peter M. Goldstein <peter.m.goldstein@gmail.com>
2015-03-20 14:43:08 -0700

    Added a couple of utilities to support IDNA domains.  Ensure that we're comparing normalized domains.  Add parent and normalization methods

--- lib/coppertone/utils/domain_utils.rb
+++ lib/coppertone/utils/domain_utils.rb
@@ -7,7 +7,7 @@ module Coppertone
     class DomainUtils
       def self.valid?(domain)
         return false if domain.blank?
-        labels = to_labels(domain)
+        labels = to_ascii_labels(domain)
         return false if labels.length <= 1
         return false if domain.length > 253
         return false if labels.any? { |l| !valid_label?(l) }
@@ -15,7 +15,22 @@ module Coppertone
       end
 
       def self.to_labels(domain)
-        Addressable::IDNA.to_ascii(domain).split('.')
+        domain.split('.')
+      end
+
+      def self.parent_domain(domain)
+        labels = to_labels(domain)
+        return '.' if labels.size == 1
+        labels.shift
+        return labels.join('.')
+      end
+
+      def self.to_ascii_labels(domain)
+        Addressable::IDNA.to_ascii(domain).split('.').map { |d| d.downcase }
+      end
+
+      def self.normalized_domain(domain)
+        to_ascii_labels(domain).join('.')
       end
 
       NO_DASH_REGEXP = /\A[a-zA-Z0-9]*[a-zA-Z]+[a-zA-Z0-9]*\z/
@@ -28,7 +43,7 @@ module Coppertone
 
       def self.valid_ldh_domain?(domain)
         return false unless valid?(domain)
-        to_labels(domain).all? { |l| valid_hostname_label?(l) }
+        to_ascii_labels(domain).all? { |l| valid_hostname_label?(l) }
       end
 
       def self.valid_label?(l)
@@ -37,7 +52,7 @@ module Coppertone
 
       def self.macro_expanded_domain(domain)
         return nil if domain.blank?
-        labels = to_labels(domain)
+        labels = to_ascii_labels(domain)
         domain = labels.join('.')
         while domain.length > 253
           labels = labels.drop(1)
@@ -47,8 +62,8 @@ module Coppertone
       end
 
       def self.subdomain_of?(subdomain_candidate, domain)
-        subdomain_labels = to_labels(subdomain_candidate)
-        domain_labels = to_labels(domain)
+        subdomain_labels = to_ascii_labels(subdomain_candidate)
+        domain_labels = to_ascii_labels(domain)
         num_labels_in_domain = domain_labels.length
         return false if subdomain_labels.length <= domain_labels.length
         subdomain_labels.last(num_labels_in_domain) == domain_labels
@@ -56,7 +71,7 @@ module Coppertone
 
       def self.subdomain_or_same?(candidate, domain)
         return false unless valid?(domain) && valid?(candidate)
-        return true if domain == candidate
+        return true if normalized_domain(domain) == normalized_domain(candidate)
         subdomain_of?(candidate, domain)
       end
     end
