c5eed8eb1c7f7ea9cf95296f173d26ab3d61d374
Peter M. Goldstein <peter.m.goldstein@gmail.com>
2016-10-17 16:06:25 -0700

    Fix use of negative?

--- lib/coppertone/mechanism/cidr_parser.rb
+++ lib/coppertone/mechanism/cidr_parser.rb
@@ -5,7 +5,7 @@ module Coppertone
       def self.parse(raw_length, max_val)
         return if raw_length.blank?
         length_as_i = raw_length.to_i
-        if length_as_i.negative? || length_as_i > max_val
+        if length_as_i < 0 || length_as_i > max_val
           raise Coppertone::InvalidMechanismError
         end
         length_as_i
